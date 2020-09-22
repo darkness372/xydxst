29076e68f14ddd454e311abf1abad61b68af229c
muyu <372681428@qq.com>
2020-09-22 10:57:12 +0800

    实现网关的应用

---
+++ xst-gateway/src/main/java/com/edu118/config/xstCorsFilter.java
@@ -0,0 +1,26 @@
+package com.edu118.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+/**
+ * @Author darkness
+ * @Date 2020/9/22 9:50
+ * @Package com.edu118.config
+ */
+@Configuration
+public class xstCorsFilter {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsWebFilter(source);
+    }
+}
