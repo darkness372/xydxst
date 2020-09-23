1cc4d453211e57f4fde2f38ac08e2e5ada2f88d6
EvenNearBoatWater <1225001172@qq.com>
2020-09-23 21:39:27 +0800

    v1.2.1  在user中进行了级联查询

--- xst-user/src/main/java/com/edu118/user/controller/UserController.java
+++ xst-user/src/main/java/com/edu118/user/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.edu118.user.controller;
 
-import java.sql.Wrapper;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.edu118.user.dao.UserDao;
+import com.edu118.common.entity.product.ProductEntity;
 import org.apache.dubbo.config.annotation.Reference;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,10 +38,8 @@ public class UserController {
     @RequestMapping("/findAll")
     public R findAll(){
         List<UserEntity> userEntity = userService.getUserAllProduct();
-        for (UserEntity ue : userEntity) {
-            System.out.println(ue);
-        }
-        return R.ok().put("page", userEntity);
+
+        return R.ok().put("data", userEntity);
     }
 
     /**
